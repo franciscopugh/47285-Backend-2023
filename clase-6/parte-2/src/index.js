387803bf4aee644c8a2b68d16c72eecf24510a07
Francisco Pugh <franciscopugh01@gmail.com>
2023-08-19 14:26:53 -0300

    Clase 6 Parte 2 A

---
+++ clase-6/parte-2/src/index.js
@@ -0,0 +1,93 @@
+import express from 'express'
+import multer from 'multer'
+import prodsRouter from "./routes/products.routes.js";
+import { __dirname } from './path.js';
+import { engine } from 'express-handlebars';
+import { Server } from 'socket.io';
+import path from 'path';
+
+const PORT = 4000
+const app = express()
+
+//Config
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'src/public/img') //null hace referencia a que no envia errores
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}${file.originalname}`) //Concateno el nombre original de mi archivo con milisegundos con Date.now()
+    }
+})
+
+const serverExpress = app.listen(PORT, () => {
+    console.log(`Server on port ${PORT}`)
+})
+
+//Middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.engine('handlebars', engine()) //Defino que motor de plantillas voy a utilizar y su config
+app.set('view engine', 'handlebars') //Setting de mi app de hbs
+app.set('views', path.resolve(__dirname, './views')) //Resolver rutas absolutas a traves de rutas relativas
+const upload = multer({ storage: storage })
+app.use('/static', express.static(path.join(__dirname, '/public'))) //Unir rutas en una sola concatenandolas
+
+//Server Socket.io
+const io = new Server(serverExpress)
+const mensajes = []
+io.on('connection', (socket) => {
+    console.log("Servidor Socket.io conectado")
+    socket.on('mensajeConexion', (user) => {
+        if (user.rol === "Admin") {
+            socket.emit('credencialesConexion', "Usuario valido")
+        } else {
+            socket.emit('credencialesConexion', "Usuario no valido")
+        }
+    })
+
+    socket.on('mensaje', (infoMensaje) => {
+        console.log(infoMensaje)
+        mensajes.push(infoMensaje)
+        socket.emit('mensajes', mensajes)
+    })
+
+})
+
+//Routes
+
+app.use('/api/products', prodsRouter)
+
+app.get('/static', (req, res) => {
+    res.render('chat', {
+        css: "style.css",
+        title: "Chat",
+
+    })
+})
+/*app.get('/static', (req, res) => {
+    const user = {
+        nombre: "Maria",
+        cargo: "Tutor"
+    }
+
+    const cursos = [
+        { numCurso: 123, dia: "S", horario: "Mañana" },
+        { numCurso: 456, dia: "MyJ", horario: "Tarde" },
+        { numCurso: 789, dia: "LyM", horario: "Noche" }
+    ]
+    res.render('home', {
+        user: user,
+        css: "style.css",
+        title: "Home",
+        esTutor: user.cargo === "Tutor",
+        cursos: cursos
+    })
+})*/
+
+app.post('/upload', upload.single('product'), (req, res) => {
+    console.log(req.file)
+    console.log(req.body)
+    res.status(200).send("Imagen cargada")
+})
+
---
+++ clase-6/parte-2/src/public/js/script.js
@@ -0,0 +1,43 @@
+/*const socket = io()
+
+socket.emit('mensajeConexion', { user: "Francisco", rol: "User" })
+
+socket.on('credencialesConexion', (info) => {
+    console.log(info)
+})*/
+
+const socket = io()
+
+const botonChat = document.getElementById('botonChat')
+const parrafosMensajes = document.getElementById('parrafosMensajes')
+const valInput = document.getElementById('chatBox')
+let user
+
+Swal.fire({
+    title: "Identificacion de usuario",
+    text: "Por favor ingrese su nombre de usuario",
+    input: "text",
+    inputValidator: (valor) => {
+        return !valor && "Ingrese su nombre de usuario valido"
+    },
+    allowOutsideClick: false
+}).then(resultado => {
+    user = resultado.value
+    console.log(user)
+})
+
+botonChat.addEventListener('click', () => {
+    let fechaActual = new Date().toLocaleString()
+
+    if (valInput.value.trim().length > 0) {
+        socket.emit('mensaje', { fecha: fechaActual, user: user, mensaje: valInput.value })
+        valInput.value = ""
+    }
+})
+
+socket.on('mensajes', (arrayMensajes) => {
+    parrafosMensajes.innerHTML = ""
+    arrayMensajes.forEach(mensaje => {
+        parrafosMensajes.innerHTML += `<p>${mensaje.fecha}: el usuario ${mensaje.user} escribio ${mensaje.mensaje} </p>`
+    })
+})
